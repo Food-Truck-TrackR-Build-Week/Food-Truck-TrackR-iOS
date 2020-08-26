a2e55f13f8fae26d3c9f3c11bef2cf014a243800
Waseem Idelbi <idelbiw@gmail.com>
2020-08-26 02:40:26 -0400

    manually adding files. one at a time, originally from the Waseem branch. Figured the problem might be with older commits with that branch, hoping this works

---
+++ Food Truck TrackR/Food Truck TrackR/Resources/String+Keys.swift
@@ -0,0 +1,19 @@
+//
+//  String+Keys.swift
+//  Food Truck TrackR
+//
+//  Created by Waseem Idelbi on 8/19/20.
+//  Copyright © 2020 Juan M Mariscal. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    static let loginViewController     =  "LoginViewController"
+    static let operatorLoginSegue      =  "OperatorLoginSegue"
+    static let dinerLoginSegue         =  "DinerLoginSegue"
+    static let loginStoryboardSegue    =  "LoginStoryboardSegue"
+    
+    
+}
